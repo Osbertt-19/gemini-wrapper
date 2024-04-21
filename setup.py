1ce3f5fd8a160741e66b2afdbc0c8758caea2eba
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2024-04-21 15:49:23 +0630

    minor release

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="geminiwrapper",
-    version="0.0.3",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "google-generativeai",
     ],
     description="Library for generating consistent Gemini API outputs and wrapping them with json and python objects",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Min Htet Naing",
     author_email="minhtetnaing25mhn@gmail.com",
     url="https://github.com/Osbertt-19/gemini-wrapper",
