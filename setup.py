56630641da18b39e7ef62e7de369a49c5364ef96
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2024-04-21 15:39:09 +0630

    added long description

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import pathlib
 from setuptools import setup, find_packages
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="geminiwrapper",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "google-generativeai",
     ],
     description="Library for generating consistent Gemini API outputs and wrapping them with json and python objects",
+    long_description=long_description,
     author="Min Htet Naing",
     author_email="minhtetnaing25mhn@gmail.com",
     url="https://github.com/Osbertt-19/gemini-wrapper",
